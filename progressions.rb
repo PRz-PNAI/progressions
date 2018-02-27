07026cb9bf0de8850cf615a8b7a5c6a6a361f039
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-02-27 16:51:40 +0100

    Fiboinacci działa!

--- progressions.rb
+++ progressions.rb
@@ -2,5 +2,16 @@ def geometric(a, q, n)
   a * q ** (n-1)
 end
 
+
+def fib(n)
+  return 1 if n < 3
+  fib(n-1) + fib(n-2)
+end
+
+
 puts geometric(4, 2, 1)
 puts geometric(4, 2, 3)
+
+puts fib(1)
+puts fib(2)
+puts fib(7)
